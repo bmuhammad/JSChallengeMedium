e3be9bc659b302bb34c68d636d73413a020407c2
Brian <briancmuhammad@yahoo.com>
2024-07-08 18:28:47 -0400

    addarray values

--- app.js
+++ app.js
@@ -17,5 +17,18 @@ console.log(filterOutFalsy(true,"Dog"));
 console.log(arrLength([1, 2, 3]));
 console.log(arrLength([5, 0, -4, 1]));
 console.log(arrLength([]));  */
+/*
+function arrSum(arr) {
+  result = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    result = result + arr[i];
+  }
+  return result;
+}
 
+console.log(arrSum([2, 2, 2]));
+console.log(arrSum([100, 200, 500]));
+console.log(arrSum([0, -5, -10]));
+
+*/
