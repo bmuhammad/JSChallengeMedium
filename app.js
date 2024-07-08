2f18d345c8b8cccfe27842d539f08bf632fb92ab
Brian <briancmuhammad@yahoo.com>
2024-07-08 17:53:54 -0400

    Medium Q1

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("test");
+/*
+function filterOutFalsy(val1, val2){
+   
+    return !val1 ? val1: val2;
+}
+
+console.log(filterOutFalsy(0,500));
+
+console.log(filterOutFalsy(false,500));
+console.log(filterOutFalsy(true,"Dog"));
+*/
