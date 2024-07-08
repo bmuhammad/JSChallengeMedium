0a0a4596ea1b0188c483e2cba44b9b2e89cd579b
Brian <briancmuhammad@yahoo.com>
2024-07-08 18:10:45 -0400

    find length of array

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ console.log(filterOutFalsy(0,500));
 console.log(filterOutFalsy(false,500));
 console.log(filterOutFalsy(true,"Dog"));
 */
+
+/* function arrLength(arr) {
+  return arr.length;
+}
+
+console.log(arrLength([1, 2, 3]));
+console.log(arrLength([5, 0, -4, 1]));
+console.log(arrLength([]));  */
+
+
